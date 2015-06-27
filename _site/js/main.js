bf899c773732f72df1b9718d7539bd08967fa44d
sophie <soph.hwang@gmail.com>
2015-06-26 23:45:14 -0700

    Revert ca1b74d..c487cfe
    This rolls back to commit ca1b74db8ab556d89a406bf38c322c9ca4b8d069.

---
+++ _site/js/main.js
@@ -0,0 +1,73 @@
+
+
+// date counter
+	var now = new Date();
+	var start = new Date(now.getFullYear(), 0, 0);
+	var diff = now - start;
+	var oneDay = 1000 * 60 * 60 * 24;
+	var day = Math.floor(diff / oneDay);
+	$('.today').append(now.toDateString());
+    // $('.day-count').append(day);
+
+var app = angular.module('RSSFeedApp', [], function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+
+
+
+app.controller("FeedCtrl", ['$scope','FeedService', function ($scope, Feed) {
+
+        Feed.parseFeed("http://doodleandletters.tumblr.com/rss").then(function(res){
+            $scope.feeds= res.data.responseData.feed.entries;
+            $scope.latest = res.data.responseData.feed.entries[0].title.match("^[0-9]{3}")[0];
+        });
+
+
+
+        $scope.today =  day ;
+
+
+
+//feeds[0].title.match("^[0-9]{3}")
+
+}]);
+
+
+app.factory('FeedService',['$http',function($http){
+    return {
+        parseFeed : function(url){
+            return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=9&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+        }
+    }
+}]);
+
+
+app.filter('html', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}])
+
+
+//<img([\w\W]+?)/>
+// new RegExp('<img([\w\W]+?)/>', 'gi');
+
+app.filter('img', ['$sce', function ($sce) {
+    return function (text) {
+
+        //http://stackoverflow.com/questions/26962297/angularjs-regex-json-data-for-img-src-or-other-selectors
+        var imgSrc = text.match(/src="([^"]*)"/i)[1];
+        //
+
+
+        return $sce.trustAsHtml(imgSrc);
+    };
+}])
+
+
+app.filter('num', function() {
+    return function(input) {
+      return parseInt(input, 10);
+    }
+});
---
+++ about.md
@@ -0,0 +1,8 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+
+You can find the source code for the Jekyll new theme at: [github.com/jglovier/jekyll-new](https://github.com/jglovier/jekyll-new)
