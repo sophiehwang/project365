6a95c95ea2eb80e42841be1a6858c8b8a3454a3b
sophie <soph.hwang@gmail.com>
2015-07-04 11:03:47 -0700

    Revert f8b8000..808493a
    This rolls back to commit f8b80003ff42d25b32bf389300c76595ad563207.

--- _site/README.md
+++
@@ -1,6 +0,0 @@
-#### project365 counter
-
-http://sophiehwang.github.io/project365/
-Lettering post counter. Keep me on track.
-
-You may also follow my daily post at http://doodleandletters.tumblr.com
--- js/main.js
+++ js/main.js
@@ -27,9 +27,7 @@ app.controller("FeedCtrl", ['$scope','FeedService', function ($scope, Feed) {
 
         $scope.today =  day ;
 
-				$scope.diff = function{
-						return latest - today;
-				}
+
 
 //feeds[0].title.match("^[0-9]{3}")
 
